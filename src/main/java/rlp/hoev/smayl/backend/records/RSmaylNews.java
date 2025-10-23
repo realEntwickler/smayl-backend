9de4b9786587d95591ee2cbb3f83afcfb7825700
Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>
2025-10-23 21:20:36 +0200

    added RSmaylNews for handling news

---
+++ src/main/java/rlp/hoev/smayl/backend/records/RSmaylNews.java
@@ -0,0 +1,26 @@
+/*
+ *
+ *  * (c) 2025 Nils Kevin Koerting-Eberhardt (realEntwickler)
+ *  *
+ *  * File: RSmaylNews.java
+ *  * Created on: 23.10.25, 21:20
+ *  *
+ *  * This file is part of the project "smayl-backend".
+ *  *
+ *  * Licensed under the Creative Commons Attribution - NonCommercial -
+ *  * NoDerivatives 4.0 International License (CC BY-NC-ND 4.0).
+ *  *
+ *  * For more information about this license, please visit:
+ *  * https://creativecommons.org/licenses/by-nc-nd/4.0/
+ *
+ */
+
+package rlp.hoev.smayl.backend.records;
+
+import rlp.hoev.smayl.backend.interfaces.ISmaylUser;
+
+import java.util.UUID;
+
+public record RSmaylNews(String title, String description, ISmaylUser author, long creationTimestamp, UUID uniqueId) {
+
+}
